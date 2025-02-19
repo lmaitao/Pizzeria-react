dd704a30384ce0b0606cba802b68f78c9d339a59
Luis Maita <lmaitao25@gmail.com>
2025-02-19 19:30:02 -0300

    agregar pizza faltantes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -19,10 +19,28 @@ function Home() {
       img: "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fcheese-164872_640_com.jpg?alt=media&token=18b2b821-4d0d-43f2-a1c6-8c57bc388fab",
     },
     {
-      name: "Pepperoni",
+      name: "Salame",
       price: 6950,
       ingredients: ["mozzarella", "pepperoni", "orégano"],
       img: "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_com.jpg?alt=media&token=e7cde87a-08d5-4040-ac54-90f6c31eb3e3",
+    },
+    {
+      name: "Pollo Picante",
+      ingredients: ["mozzarella", "pimientos", "pollo grillé", "orégano"],
+      price: 8500,
+      img: "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-2000595_640_c.jpg?alt=media&token=61325b6e-a1e0-441e-b3b5-7335ba13e8be",
+    },
+    {
+      name: "Cuatro Estaciones",
+      ingredients: ["mozzarella", "salame", "aceitunas", "champiñones"],
+      price: 9590,
+      img: "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-2000595_640_c.jpg?alt=media&token=61325b6e-a1e0-441e-b3b5-7335ba13e8be",
+    },
+    {
+      name: "Bacon",
+      ingredients: ["mozzarella", "tomates cherry", "bacon", "orégano"],
+      price: 6450,
+      img: "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-salame.jpg?alt=media&token=ab3d4bf8-01f2-4810-982b-bd7fb6b517b2",
     },
   ];
 
