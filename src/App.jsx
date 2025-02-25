12d19a454edaad989d3601b990c248087733a125
Luis Maita <lmaitao25@gmail.com>
2025-02-25 10:47:26 -0300

    modificacion en router
    se realiza cambio para mostrar el contenido completo

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,23 @@ import Pizzas from './components/Pizzas/Pizzas'
 import Footer from "./components/Footer/Footer";
 import Register from './components/Register/Register';
 import Login from "./components/Login/Login";
-import { BrowserRouter as Router, Routes, Route, BrowserRouter,} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route,} from 'react-router-dom';
 
 
 
 const App = () => {
   return (
     <>
-      <BrowserRouter />
         <Router>
           <NavBar />
           <Routes>
+              <Route path="/" element={<Pizzas />} />
               <Route path="/home" element={<Pizzas />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
           </Routes>
         </Router>
         <Footer />
-      <BrowserRouter />
     </>
   );
 };
