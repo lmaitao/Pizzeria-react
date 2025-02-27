3232d9d887529090575b0ff1ce5ed47307266b34
Luis Maita <lmaitao25@gmail.com>
2025-02-27 17:51:29 -0300

    componente header
    se modifica nombre

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,11 +5,11 @@ function Header() {
     <header className="jumbotron text-center" id="header-image">
       <div className="container">
         <h1 className="display-4 fw-bold text-white">
-          ¡Bienvenido a Mi Proyecto!
+          ¡Bienvenido a la Pizzeria Mamma Mia el Mejor Sabor de Chile!
         </h1>
         <p className="lead text-white">
-          Esta es la página de inicio de tu proyecto. Aquí encontrarás
-          información relevante sobre nuestros productos y servicios.
+          Aquí encontrarás información relevante sobre nuestros
+          productos de alta calidad y los mejores servicios.
         </p>
       </div>
     </header>
