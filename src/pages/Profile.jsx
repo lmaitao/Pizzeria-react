662eb4393987d6193885c51cf5b3bbd96daafe62
Luis Maita <lmaitao25@gmail.com>
2025-03-18 20:02:10 -0300

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,23 @@
-import '../components/Profile/Profile.css'
+import '../components/Profile/Profile.css';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { UserContext } from '../components/Profile/Usercontext';
 
-function Profile({ onLogout }) {
+function Profile() {
   const staticEmail = 'usuario@ejemplo.com';
+  const navigate = useNavigate();
+  const { logout } = useContext(UserContext);
+
+  const handleLogout = () => {
+    logout();
+    navigate('/');
+  };
 
   return (
     <div className="profile-container">
       <h2>Perfil de Usuario</h2>
       <p>Email: {staticEmail}</p>
-      <button className="logout-button" onClick={onLogout}>
+      <button className="logout-button" onClick={handleLogout}>
         Cerrar Sesión
       </button>
     </div>
